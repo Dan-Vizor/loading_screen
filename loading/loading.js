61e860ceefe65063ccb3119c588630f49241e6bc
Daniel Vizor <danielv995.dv@gmail.com>
2018-11-17 15:08:48 +0000

    update

--- loading/loading.js
+++ loading/loading.js
@@ -5,11 +5,11 @@ function setup() {
 
 var time = 0;
 var x = CanvasSize/2;
-var y = 0;
+var y = CanvasSize/2;
 function draw() {
     background('#FFFFFF');
     fill(color(100,100,255));
-    y = 150 * Math.sin(time / 20)
-    ellipse(x, y + 250, 50 * Math.sin(time / 20), 50);
+    y = CanvasSize/5 * Math.sin(time / 20)
+    ellipse(x, y + CanvasSize/3, 50 * Math.sin(time / 20), 50);
     time += 1;
 }
